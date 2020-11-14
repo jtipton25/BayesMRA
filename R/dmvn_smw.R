53796ea10ffd6bdc4a6fd5de6746fbc2849b12ea
jtipton25 <jtipton25@users.noreply.github.com>
2020-11-13 20:11:55 -0600

    starting to build out integrated model

---
+++ R/dmvn_smw.R
@@ -0,0 +1,48 @@
+#' dmvn_smw_mra
+#'
+#' @param y is a \eqn{n}{n} vector of Gaussian data.
+#' @param X is a \eqn{n \times p}{n x p} matrix of fixed effects (like latitude, elevation, etc)
+#' @param beta is the regression parameter beta
+#' @param tW is the transpose of the sparse Wendland basis matrix of class spam
+#' @param tWW is the transpose of the sparse Wendland basis matrix multiplied by itself of class spam
+#' @param Q_alpha_tau2 is the prior precision matrix for random effects alpha
+#' @param sigma2 is the residual error
+#' @param Rstruct is the Cholesky prior precision matrix for random effects alpha
+#' @param logd
+#'
+#' @return
+#' @import spam
+#' @export
+#'
+#' @examples
+dmvn_smw <- function(y, X, beta, tW, tWW, Q_alpha_tau2, sigma2, Rstruct = NULL, logd = TRUE) {
+    n <- length(y)
+    I <- spam_diag(n)
+
+    devs <- y - X %*% beta
+    ## calculate the Sherman-Morrison-Woodbury inverse
+
+    G <- Q_alpha_tau2 + tWW / sigma2
+    G_chol <- NULL
+    if (is.null(Rstruct)) {
+        G_chol <- chol(G)
+    } else {
+        G_chol <- update.spam.chol.NgPeyton(Rstruct, G)
+    }
+
+    ## calculate the Sherman-Morrison-Woodbury determinant
+    Sigma_det <- 2.0 * sum(log(diag(G_chol))) -
+        determinant(Q_alpha_tau2, logartithm = TRUE)$modulus[1] +
+        n * log(sigma2)
+
+    ## calculate the Sherman-Morrison-Woodbury log likelihood
+    ll <- - 0.5 * n * log(2 * base::pi) -
+                 0.5 * Sigma_det -
+                 0.5 / sigma2 * (sum(devs^2) - sum(forwardsolve(G_chol, transpose = TRUE, tW %*% devs, upper.tri = TRUE)^2) / sigma2)
+
+    if (logd) {
+        return(ll)
+    } else {
+        return(exp(ll))
+    }
+}
